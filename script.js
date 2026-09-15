5db39296c2dddf1eee9bd6e1474acca0bbebc242
Subhan9111 <monea007@osloskolen.no>
2026-09-15 14:12:21 +0200

    jeg har startet på javascript nå

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 const form =document.getElementById("budget-form")
 const list =document.getElementById("budget-list")
+
+let totalincome=0;
+let totalexpenses=0;
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    console.log("knappen fungerer!")
+});
+
+const description= document.getElementById("description").value;
+const amount= document.getElementById("amount").value;
+const type= document.getElementById("type").value;
+const category= document.getElementById("category").value;
+
+console.log(description);
+console.log(amount);
+console.log(type);
+console.log(category);
+
