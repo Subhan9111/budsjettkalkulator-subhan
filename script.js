5a87d79353da1fdf1293381beb4797e4fa6f9a92
Subhan9111 <monea007@osloskolen.no>
2026-09-16 14:18:34 +0200

    jeg er ferdig med js men tenker å vidre utvikle den imorgen

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const form =document.getElementById("budget-form")
 const list =document.getElementById("budget-list")
 
-let totalincome=0;
-let totalexpenses=0;
+let totalIncome=0;
+let totalExpenses=0;
 
 form.addEventListener("submit", function(event) {
     event.preventDefault();
     console.log("knappen fungerer!")
-});
+
 
 const description= document.getElementById("description").value;
 const amount= document.getElementById("amount").value;
@@ -19,3 +19,31 @@ console.log(amount);
 console.log(type);
 console.log(category);
 
+const item = document.createElement ("li");
+item.textContent = description +"-"+ category+ ":" + amount+"kr";
+list.appendChild(item);
+
+if (type=== "income") {
+    totalIncome += Number (amount);
+} else {
+    totalExpenses += Number (amount);
+}
+ document.getElementById("total-income").textContent =
+ totalIncome + "kr";
+
+ document.getElementById("total-expenses").textContent =
+ totalExpenses + "kr";
+
+ const balance = totalIncome - totalExpenses
+
+ document.getElementById("balance").textContent =
+ balance + "kr";
+
+ if(balance < 0) {
+    document.getElementById("balance").style.color = "red"
+ } else {
+    document.getElementById("balance").style.color = "black"
+ }
+
+ form.reset ();
+ });
